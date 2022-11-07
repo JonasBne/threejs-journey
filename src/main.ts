b1434c448d5dfb6c6deb585743d6d8092aaf44c6
Jonas <boone.jonas@outlook.be>
2022-11-07 20:30:12 +0100

    chapter1/09-geometries-final

--- src/main.ts
+++ src/main.ts
@@ -17,26 +17,8 @@ const canvas = document.getElementsByClassName('webgl')[0] as HTMLCanvasElement;
 
 const scene = new THREE.Scene();
 
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-
-// 8 vertices consisting out of 3 points in 3d space along x, y and z axis
-const positions = new Float32Array([
-    0, 0, 0, // vertex1
-    1, 0, 0, // vertex2
-    0, 1, 0, // vertex3
-    1, 1, 0, // vertex4
-    0, 0, 1, // vertex5
-    0, 1, 1, // vertex6
-    1, 0, 1, // vertex7
-    1, 1, 1, // vertex8
-])
-// 3 points per vertex to create the face
-const positionsAttribute = new THREE.BufferAttribute(positions, 3);
-
-const geometry = new THREE.BufferGeometry();
-geometry.setAttribute('position', positionsAttribute);
-
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000'});
+const geometry = new THREE.BoxGeometry(1, 1, 1, 3, 3, 3);
+const material = new THREE.MeshBasicMaterial({ color: '#ff0000', wireframe: true});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
