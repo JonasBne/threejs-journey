31a273adbc356808ae07269e8fe568d5fbe48987
Jonas <boone.jonas@outlook.be>
2022-11-29 14:28:34 +0100

    chapter2/lights-final

--- src/main.ts
+++ src/main.ts
@@ -1,68 +1,48 @@
-import './style.css'
+import './style.css';
 import * as THREE from 'three';
-import { OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-import {FontLoader} from "three/examples/jsm/loaders/FontLoader";
-import {TextGeometry} from "three/examples/jsm/geometries/TextGeometry";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import GUI from 'lil-gui';
+
+const gui = new GUI();
 
 // canvas
 const canvas = document.getElementsByClassName('webgl')[0] as HTMLCanvasElement;
 
 const scene = new THREE.Scene();
 
-// fonts
-const fontLoader = new FontLoader();
-fontLoader.load('src/assets/fonts/helvetiker_regular.typeface.json', (font) => {
-    const textGeometry = new TextGeometry('Hello World', {
-        font,
-        size: 0.5,
-        height: 0.2,
-        curveSegments: 5,
-        bevelEnabled: true,
-        bevelThickness: 0.03,
-        bevelSize: 0.02,
-        bevelOffset: 0,
-        bevelSegments: 4,
-    });
-
-    textGeometry.center()
-
-    const material = new THREE.MeshNormalMaterial();
-
-    const text = new THREE.Mesh(
-        textGeometry,
-        material,
-    )
-    scene.add(text);
-})
-
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
 window.addEventListener('resize', () => {
-    // update sizes of the screen
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
+  // update sizes of the screen
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // update camera aspect and projection
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
+  // update camera aspect and projection
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // update size of renderer
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // limit to pixel ratio of 2 to reduce the effort on (mobile) devices
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-})
+  // update size of renderer
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // limit to pixel ratio of 2 to reduce the effort on (mobile) devices
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 window.addEventListener('dblclick', () => {
-    if (!document.fullscreenElement) {
-        return canvas.requestFullscreen();
-    }
-    return document.exitFullscreen();
-})
+  if (!document.fullscreenElement) {
+    return canvas.requestFullscreen();
+  }
+  return document.exitFullscreen();
+});
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100,
+);
 camera.position.z = 5;
 // move the camera, otherwise everything is centered in the middle of the scene and nothing will be visible
 scene.add(camera);
@@ -73,45 +53,64 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 const renderer = new THREE.WebGLRenderer({
-    canvas
+  canvas,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 // limit to pixel ratio of 2 to reduce the effort on (mobile) devices
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// create random objects
-const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+// objects
+// Material
+const material = new THREE.MeshStandardMaterial();
+material.roughness = 0.4;
 
-for (let i=0; i<100; i++) {
-    let donut = new THREE.Mesh(donutGeometry, new THREE.MeshNormalMaterial());
+// Objects
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
+sphere.position.x = -1.5;
 
-    const scale = Math.random();
-    donut.scale.set(scale, scale, scale);
+const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material);
 
-    donut.position.x = (Math.random() - 0.5) * 10
-    donut.position.y = (Math.random() - 0.5) * 10
-    donut.position.z = (Math.random() - 0.5) * 10
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+  material,
+);
+torus.position.x = 1.5;
 
-    donut.rotation.x = Math.random();
-    donut.rotation.y = Math.random()
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
+plane.rotation.x = -Math.PI * 0.5;
+plane.position.y = -0.65;
 
-    scene.add(donut)
-}
+scene.add(sphere, cube, torus, plane);
+
+// lights
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+// gui.add(ambientLight, 'intensity', 0, 1, 0.001);
+// scene.add(ambientLight);
+// const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+// gui.add(directionalLight, 'intensity', 0, 1, 0.001);
+// gui.add(directionalLight.position, 'x', -2, 2, 0.01);
+// gui.add(directionalLight.position, 'y', -2, 2, 0.01);
+// gui.add(directionalLight.position, 'z', -2, 2, 0.01);
+// scene.add(directionalLight);
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2);
+gui.add(pointLight.position, 'x', -2, 2, 0.01);
+gui.add(pointLight.position, 'y', -2, 2, 0.01);
+gui.add(pointLight.position, 'z', -2, 2, 0.01);
+gui.add(pointLight, 'intensity', 0, 1, 0.01);
+gui.add(pointLight, 'distance', 0, 4, 0.01);
+gui.add(pointLight, 'decay', 0, 5, 0.01);
+scene.add(pointLight);
 
 // animations
 const animate = () => {
-    // must be added if damping is required
-    controls.update();
+  // must be added if damping is required
+  controls.update();
 
-    // render the scene
-    renderer.render(scene, camera);
+  // render the scene
+  renderer.render(scene, camera);
 
-    // pass reference to the function that calls window.requestAnimationFrame
-    window.requestAnimationFrame(animate)
-}
+  // pass reference to the function that calls window.requestAnimationFrame
+  window.requestAnimationFrame(animate);
+};
 
 animate();
-
-
-
-
