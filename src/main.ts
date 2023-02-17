56ebf0c6852506e4a847a4a843da1872d23318c9
Jonas Boone <boone.jonas@outlook.be>
2023-02-17 19:06:58 +0100

    chapter2/shadows-starter-setup

--- src/main.ts
+++ src/main.ts
@@ -43,6 +43,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100,
 );
+camera.position.y = 2;
 camera.position.z = 5;
 // move the camera, otherwise everything is centered in the middle of the scene and nothing will be visible
 scene.add(camera);
@@ -59,47 +60,36 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // limit to pixel ratio of 2 to reduce the effort on (mobile) devices
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// objects
-// Material
+/*
+ * lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(2, 2, -1);
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001);
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001);
+scene.add(directionalLight);
+
+/*
+ * objects
+ */
 const material = new THREE.MeshStandardMaterial();
-material.roughness = 0.4;
+material.roughness = 0.7;
+gui.add(material, 'metalness').min(0).max(1).step(0.001);
+gui.add(material, 'roughness').min(0).max(1).step(0.001);
 
-// Objects
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
-sphere.position.x = -1.5;
-
-const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material);
-
-const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(0.3, 0.2, 32, 64),
-  material,
-);
-torus.position.x = 1.5;
 
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
 plane.rotation.x = -Math.PI * 0.5;
-plane.position.y = -0.65;
+plane.position.y = -0.75;
 
-scene.add(sphere, cube, torus, plane);
-
-// lights
-// const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-// gui.add(ambientLight, 'intensity', 0, 1, 0.001);
-// scene.add(ambientLight);
-// const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-// gui.add(directionalLight, 'intensity', 0, 1, 0.001);
-// gui.add(directionalLight.position, 'x', -2, 2, 0.01);
-// gui.add(directionalLight.position, 'y', -2, 2, 0.01);
-// gui.add(directionalLight.position, 'z', -2, 2, 0.01);
-// scene.add(directionalLight);
-const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2);
-gui.add(pointLight.position, 'x', -2, 2, 0.01);
-gui.add(pointLight.position, 'y', -2, 2, 0.01);
-gui.add(pointLight.position, 'z', -2, 2, 0.01);
-gui.add(pointLight, 'intensity', 0, 1, 0.01);
-gui.add(pointLight, 'distance', 0, 4, 0.01);
-gui.add(pointLight, 'decay', 0, 5, 0.01);
-scene.add(pointLight);
+scene.add(sphere, plane);
 
 // animations
 const animate = () => {
